7332ad860dd53f68d009d9d3df4a5bcfcd3a28bc
itzme <itzmenaveen07@gmail.com>
2026-03-04 18:02:16 +0530

    filtered

--- javabasic/src/Main.java
+++ javabasic/src/Main.java
@@ -1,88 +1,59 @@
 //import java.util.Scanner;
 
+import org.w3c.dom.css.Counter;
+
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to java!");
 
-//   System.out.println("call a method using static and Non-static");
-//   StaticPractice.A();// calling using main class directly using a method name because it is static 'or' we can also call using class name.method name
-//   StaticPractice obj = new StaticPractice();// creating an object for method b using main class because it is non-static
-//   obj.B();// calling a method b using the object obj and the method name
-//   obj.C();
-//   ControlStatement.Statement();
-//   Scanner Number = new Scanner(System.in);
-//   System.out.print("Enter a number: ");
-//   int num1 = Number.nextInt();
-//   System.out.print("Enter another number: ");
-//   int num2 =Number.nextInt();
-//   int sum = Functions.add(num1, num2);
-//   System.out.println("The sum of "+num1 +" and "+num2 +" is "+sum +".");
-//   System.out.println("Date and time");
-//   CurrentDateTime.date();
-//   System.out.println("Data structures");
-//   DataStructures.ArrayExample();
-//   DataStructures.ArrayListExample();
-//   DataStructures.LinkedList1();
-//   DataStructures.stack();
-//   DataStructures.hashmap();
-//   DataStructures.hashset();
-//   DataStructures.treeset();
-//   DataStructures.treemap();
-//   DataStructures t= new DataStructures();
-//   t.tree();
-//   Generics.gen();
-//   stream.filter();
-//   stream obj= new stream();
-//   Employee.streamEmployee();
-//   obj.filterusinghash();
-//   stream.map ();
-//   stream.mapusinghash();
-//   stream.sortlist();
-//   Dog d = new Dog("Tommy");
-//   d.speak(); // Output: Tommy barks
-//   Bird.parrot();//
-//   TwoSumNumber.Sums();
-//   TwoSumNumber.Anagram();
-//   Strings.Returnnonrepeated();
-//   Strings.Reversewithbuilder();
-//   Strings.Reversewithoutstringbuilder();
-//   Strings.palindrome();
-//   Strings.palindrome1();
-//   Strings.CountVowelsAndConsonants();
-//   Strings.Removespaces();
- //  Strings.stringToInteger();
-//   Strings.countVowelsAndConsonants();
-//   Strings.parantheses();
-//   Functions.charc();
-//   Runnable obj1 = new A();
-//   Runnable obj2 = new B();
-//   A obj1=new A();
-//   B obj2=new B();
-//   obj1.show();
-//   obj2.show();
-//   obj1.start();
-//   obj2.start();
-//   Counter c = new Counter();
-//   System.out.println(c.count);
-//   Animals a = new Dogs();   // Allowed
-//   a.sound();
-//  Parent p = new Child();
-//  p.show();
-//  second.test();
-//  second.tests();
-//  test.nextinteger();
-//   zoho.questions();
-//  FilesExample.createFile();
-        DatabaseConnection db = DatabaseConnection.getInstance();
-//        db.connect();
-//        db.connected();
+   System.out.println("call a method using static and Non-static");
+   StaticPractice.A();// calling using main class directly using a method name because it is static 'or' we can also call using class name.method name
+   StaticPractice obj = new StaticPractice();// creating an object for method b using main class because it is non-static
+   obj.B();// calling a method b using the object obj and the method name
+   obj.C();
+   ControlStatement.Statement();
+   Scanner Number = new Scanner(System.in);
+   System.out.print("Enter a number: ");
+   int num1 = Number.nextInt();
+   System.out.print("Enter another number: ");
+   int num2 =Number.nextInt();
+   System.out.println("The sum of "+num1 +" and "+num2 +" is "+".");
+   System.out.println("Date and time");
+   CurrentDateTime.date();
+   System.out.println("Data structures");
+   DataStructures.ArrayListExample();
+   DataStructures.LinkedList1();
+   DataStructures.hashmap();
+   DataStructures.treemap();
+   DataStructures t= new DataStructures();
+   t.tree();
+   Employee.streamEmployee();
+   TwoSumNumber.Sums();
+   TwoSumNumber.Anagram();
+   Strings.Returnnonrepeated();
+   Strings.Reversewithbuilder();
+   Strings.Reversewithoutstringbuilder();
+   Strings.palindrome();
+   Strings.palindrome1();
+   Strings.CountVowelsAndConsonants();
+   Strings.Removespaces();
+   Strings.stringToInteger();
+   Strings.countVowelsAndConsonants();
+   Strings.parantheses();
+   Functions.charc();
+   second.test();
+   second.tests();
+   zoho.questions();
+   FilesExample.createFile();
+   DatabaseConnection db = DatabaseConnection.getInstance();
+   db.connect();
+   db.connected();
 
     }
-
-
 }
 
 
